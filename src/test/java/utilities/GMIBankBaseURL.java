272e17bc968fa13f412c692f5aa9461464c46be8
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-01-05 13:44:25 +0300

    v10

--- src/test/java/utilities/GMIBankBaseURL.java
+++ src/test/java/utilities/GMIBankBaseURL.java
@@ -1,8 +1,8 @@
 package utilities;
 
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.specification.RequestSpecification;
-import org.junit.Before;
+import io.restassured.builder.*;
+import io.restassured.specification.*;
+import org.junit.*;
 
 public class GMIBankBaseURL extends Authentication{
 
