246791042cfd3be9f127a75502b970cfa53cd096
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-01-05 13:44:04 +0300

    v9

--- src/test/java/utilities/DatabaseUtility.java
+++ src/test/java/utilities/DatabaseUtility.java
@@ -1,10 +1,7 @@
 package utilities;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class DatabaseUtility {
     private static Connection connection;
